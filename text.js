fa55afa26a9b79de8399f9e166bb805359b7b0dc
raquelarcocalderon-prog <raquelarcocalderon@gmail.com>
2026-04-20 22:32:19 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es" id="exe-page-1776714727470-emp260349">
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="eXeLearning v4.0.0-rc1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="license" type="text/html" href="https://creativecommons.org/licenses/by-sa/4.0/">
+<title>Guía didáctica</title>
+<link rel="icon" type="image/x-icon" href="../libs/favicon.ico">
+<meta name="description" content="Situación de Aprendizaje para descubrir el entorno más cercano (su pueblo: Manilva). De esta manera, descubriremos la gastronomía típica, los monumentos, las fiestas y las actividades de ocio y tiempo libre. ">
+<script>document.querySelector("html").classList.add("js");</script><script src="../libs/jquery/jquery.min.js"> </script><script src="../libs/common_i18n.js"> </script><script src="../libs/common.js"> </script><script src="../libs/exe_export.js"> </script><script src="../libs/bootstrap/bootstrap.bundle.min.js"> </script><link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+<script src="../idevices/text/text.js"></script><link rel="stylesheet" href="../idevices/text/text.css">
+<link rel="stylesheet" href="../content/css/base.css"><script src="../theme/style.js"> </script><link rel="stylesheet" href="../theme/style.css">
+</head>
+<body class="exe-export exe-web-site">
+<script>document.body.className+=" js"</script>
+<div class="exe-content exe-export pre-js siteNav-hidden"><a href="#page-1776714727470-emp260349" id="skipNav">Skip to content</a> <nav id="siteNav">
+<ul>
+<li> <a href="../index.html" class="main-node no-ch">Inicio</a>
+</li>
+<li> <a href="../html/curacion-de-contenidos.html" class="no-ch">Curación de contenidos</a>
+</li>
+<li> <a href="../html/licencias-y-seguridad.html" class="no-ch">Licencias y seguridad</a>
+</li>
+<li> <a href="../html/secuenciacion-didactica.html" class="no-ch">Secuenciación didáctica</a>
+</li>
+<li> <a href="../html/evaluacion.html" class="no-ch">Evaluación</a>
+</li>
+<li> <a href="../html/linea-temporal.html" class="no-ch">Línea temporal</a>
+</li>
+<li class="active"> <a href="../html/guia-didactica.html" class="active no-ch">Guía didáctica</a>
+</li>
+<li> <a href="../html/creditos.html" class="no-ch">Créditos</a>
+</li>
+</ul>
+</nav><main id="page-1776714727470-emp260349" class="page"> 
+<header class="main-header">
+<div class="package-header"><p class="package-title">Manilva mi pueblo</p></div>
+<div class="page-header"><h1 class="page-title">Guía didáctica</h1></div>
+</header><div id="page-content-page-1776714727470-emp260349" class="page-content">
+<article id="block-1776714734762-ewogj6pei" class="box">
+<header class="box-head no-icon">
+<h1 class="box-title">Texto</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1776714734760-enw2ix250" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1776714734760-enw2ix250&quot;}">
+<div class="exe-text-template">
+    <p>Relación con criterios de evaluación, las actividades están relacionadas con:</p>
+<p>-Conocimiento del entorno<br />-Expresión oral<br />-Competencia digital<br /></p>
+<p>En cuanto a la metodología se utiliza el Aprendizaje Cooperativo. diferentes metodologías activas y el uso de herramientas digitales. <br /></p>
+<p>En relación a la Atención a la diversidad se aplican los principios del DUA:</p>
+<p>-Diferentes formas de aprender<br />-Diferentes formas de expresar<br /></p>
+<p>A modo de recomendaciones:<br />-Acompañar al alumnado en el uso de la tecnología<br />-Adaptar el ritmo de trabajo</p>
+</div>
+</div>
+</div>
+</article>
+</div></main><div class="nav-buttons">
+<a href="../html/linea-temporal.html" title="Anterior" class="nav-button nav-button-left"><span>Anterior</span></a>
+<a href="../html/creditos.html" title="Siguiente" class="nav-button nav-button-right"><span>Siguiente</span></a>
+</div>
+<footer id="siteFooter"><div id="siteFooterContent"> <div id="packageLicense" class="cc cc-by-sa"> <p> <span class="license-label">Licencia: </span><a href="https://creativecommons.org/licenses/by-sa/4.0/" class="license">creative commons: attribution - share alike 4.0 (BY-SA)</a></p>
+</div>
+</div></footer>
+</div>
+<p id="made-with-eXe"> <a href="https://exelearning.net/" target="_blank" rel="noopener"> <span>Creado con eXeLearning <span>(nueva ventana)</span></span></a></p>
+</body>
+</html>
